69309b0152a4d240f9e6aa51c599b8c408c234ed
Haochen Wei <weihch90@gmail.com>
2012-11-19 14:49:00 -0800

    Small change to MainWindow design

--- WindowsFormsApplication1/MainWindow.Designer.cs
+++ WindowsFormsApplication1/MainWindow.Designer.cs
@@ -162,7 +162,7 @@
             // controlButton
             // 
             this.controlButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.controlButton.Location = new System.Drawing.Point(227, 158);
+            this.controlButton.Location = new System.Drawing.Point(229, 158);
             this.controlButton.Name = "controlButton";
             this.controlButton.Size = new System.Drawing.Size(138, 61);
             this.controlButton.TabIndex = 1;
